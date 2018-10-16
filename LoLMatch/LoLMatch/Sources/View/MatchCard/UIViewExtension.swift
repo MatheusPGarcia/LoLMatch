33aaed0a2694a83879c89d7386c873f7d97b6a9f
Scarpz <felipe.scarpz@gmail.com>
2018-10-16 15:17:47 -0300

    MatchCard visual created with a separated XIB

--- LoLMatch/LoLMatch/Sources/View/MatchCard/UIViewExtension.swift
+++ LoLMatch/LoLMatch/Sources/View/MatchCard/UIViewExtension.swift
@@ -6,4 +6,38 @@
 //  Copyright © 2018 Matheus Garcia. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UIView {
+    
+    /// Inspectable property to set the corner radius in the Storyboard
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+//            layer.masksToBounds = newValue > 0
+        }
+    }
+    
+    /// Inspectable property to set the border width in the Storyboard
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    /// Inspectable property to set the border color in the Storyboard
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            return UIColor(cgColor: layer.borderColor!)
+        }
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+}
